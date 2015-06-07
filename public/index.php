9346539d1b3aaaa06245c370f982910a84bf0136
Rich Hildred <rhildred@gmail.com>
2015-06-07 15:45:42 -0400

    changed the folder in case a trailing slash isn't good

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL | E_STRICT);
 
     $app = new \Slim\Slim(array(
         'view' => new \PHPView\PHPView(),
-        'templates.path' => __DIR__ . "/../views/"));
+        'templates.path' => __DIR__ . "/../views"));
     $app->get('/hello/:name', function ($name) {
         echo "Hello, " . $name;
     });
--- src/PHPView/PHPView.php
+++ src/PHPView/PHPView.php
@@ -5,7 +5,7 @@ namespace PHPView;
 class PHPView extends \Slim\View{
     protected function render($template, $data = null){
         $Viewbag = $this->data;
-        include($this->getTemplatesDirectory() . $template);
+        include($this->getTemplatesDirectory() . '/' . $template);
     }
 }
 
