7e07a59fe7b7b21199c5888c90177584c6637271
Richard Hildred <rhildred@gmail.com>
2015-06-08 08:59:19 -0400

    made helper also work for css and js

--- PHPView/PHPView.php
+++ PHPView/PHPView.php
@@ -3,14 +3,17 @@
 namespace PHPView;
 
 class PHPView extends \Slim\View{
+    
+    // override for Slim
     private $sPage;
     protected function render($template, $data = null){
         $this->sPage = $template;
-        $Viewbag = $this->data;
         try{
             include $this->getTemplatesDirectory() . '/' . $template;
         }catch(\Exception $e){}
     }
+
+    // helper method so that we can have a layout
     private $bInLayout = FALSE;
     protected function layout($template){
         if(!$this->bInLayout){
@@ -19,11 +22,19 @@ class PHPView extends \Slim\View{
             throw new \Exception("Don't print twice");
         }
     }
+
+    // helper method to render te original page inside the layout
     protected function renderBody(){
         include $this->getTemplatesDirectory() . '/' . $this->sPage;
     }
+
+    // helper method to create urls given slim routing
     protected function urlFor($name){
-        return preg_replace("/index.php(.*)$/", "index.php/" . $name, $_SERVER["PHP_SELF"]);
+        if(preg_match("/\.(.*)s$/", $name)){
+            return preg_replace("/index.php(.*)$/", $name, $_SERVER["PHP_SELF"]);            
+        }else{
+            return preg_replace("/index.php(.*)$/", "index.php/" . $name, $_SERVER["PHP_SELF"]);
+        }
     }
 }
 
